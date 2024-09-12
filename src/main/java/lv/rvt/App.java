cc47f9d7c82a5b79e26d53d7ebcf014d84370a66
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-09-12 11:40:40 +0000

    Pirmais hello world

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,5 +5,7 @@ public class App
     public static void main( String[] args )
     {
         // Make your Hello World programm!
+        System.out.println("Hello world!");
+        // Komentars sakas ar 2 /. 
     }
 }
