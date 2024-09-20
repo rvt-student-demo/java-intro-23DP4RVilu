8aae1e6d1aed1f84395fc0a7160cb6e2f7479456
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-09-20 10:35:01 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,32 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
-        System.out.println("Hello world!");
-        // Komentars sakas ar 2 /. 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Answers are: ");
+
+        Integer result1 = first + second;
+        System.out.println(first + " + " + second + " = " + result1);
+
+        Integer result2 = first - second;
+        System.out.println(first + " - " + second + " = " + result2);
+
+        Integer reslt3 = first * second;
+        System.out.println(first + " * " + second + " = " + reslt3);
+
+        double result4 = (double) first / second;
+        System.out.println(first + " / " + second + " = " + result4);
+        
     }
 }
