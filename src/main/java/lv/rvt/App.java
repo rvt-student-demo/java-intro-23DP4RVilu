7fc074dab0dd650efd7dd3b2d497ef0355cad8c1
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-09-27 11:06:11 +0000

    Grades and points v2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -15,27 +15,30 @@ public class App
             System.out.println("Grade: Incredible!");
         }
 
-        else if (points < 100 && points > 90) {
+        else if (points >= 90) {
             System.out.println("Grade: 5");
         }
 
-        else if (points < 89 && points > 80) {
-            System.out.println("Grade: 4");
+        else if (points >= 80) {
+            System.out.println("4");
         }
 
-        else if (points < 79 && points > 70) {
-            System.out.println("Grade: 3");
+        else if (points >= 70) {
+            System.out.println("3");
         }
 
-        else if (points < 69 && points > 60) {
-            System.out.println("Grade: 2");
+        else if (points >= 60) {
+            System.out.println("2");
         }
 
-        else if (points < 59 && points > 50) {
-            System.out.println("Grade: 1");
+        else if (points >= 50) {
+            System.out.println("1");
         }
 
-        else if (points < 49 && points > 0) {
+        else if (points == 49) {
+            System.out.println("Failed");
+        }
+        else if (points < 49 && points > 0 ) {
             System.out.println("Failed");
         }
 
