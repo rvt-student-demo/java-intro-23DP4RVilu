bb5315d91725ae1274c83536c823463f27bbb30b
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-09-27 11:36:52 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,43 +8,29 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give points [0-100] ");
-        int points = Integer.valueOf(scanner.nextLine());
+        System.out.println("Value of gift?");
+        int value = Integer.valueOf(scanner.nextLine());
+        double tax;
 
-        if (points > 100) {
-            System.out.println("Grade: Incredible!");
-        }
-
-        else if (points >= 90) {
-            System.out.println("Grade: 5");
-        }
-
-        else if (points >= 80) {
-            System.out.println("4");
-        }
-
-        else if (points >= 70) {
-            System.out.println("3");
-        }
-
-        else if (points >= 60) {
-            System.out.println("2");
-        }
-
-        else if (points >= 50) {
-            System.out.println("1");
+        if (value >= 5000 && value < 25000) {
+            tax = (100 + (value - 5000) * 0.08);
+            System.out.println(tax);
+        } else if (value >= 2500 && value < 55000) {
+            tax = (1700 + (value - 25000) * 0.10);
+            System.out.println(tax);
+        } else if (value >= 55000 && value < 200000) {
+            tax = (4700 + (value - 55000) * 0.12);
+            System.out.println(tax);
+        } else if (value >= 200000 && value < 1000000) {
+            tax = (22100 + (value - 20000) * 0.15);
+            System.out.println(tax); 
+        } else if (value >= 1000000) {
+            tax = (142100 + (value - 1000000) * 0.17);
+            System.out.println(tax); 
+        }  else {
+            System.out.println("No tax!");
         }
 
-        else if (points == 49) {
-            System.out.println("Failed");
-        }
-        else if (points < 49 && points > 0 ) {
-            System.out.println("Failed");
-        }
-
-        else if (points <= 0) {
-            System.out.println("Grade impossible!");
-        }
     }
     
 }
