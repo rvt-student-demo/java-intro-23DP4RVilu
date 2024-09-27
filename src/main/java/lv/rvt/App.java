010b5c70e2c3eeb7087654c80dd4f19e0da8f20c
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-09-27 11:01:45 +0000

    Grades and points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,18 +8,39 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give speed: ");
-        int speed = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give points [0-100] ");
+        int points = Integer.valueOf(scanner.nextLine());
 
-        if (speed > 120) {
-            System.out.println("Speeding Ticket!");
+        if (points > 100) {
+            System.out.println("Grade: Incredible!");
         }
 
-        if (speed == 120) {
-            System.out.println("Speeding Ticket!");
+        else if (points < 100 && points > 90) {
+            System.out.println("Grade: 5");
         }
-        else {
-            System.out.println("No Ticket.");
+
+        else if (points < 89 && points > 80) {
+            System.out.println("Grade: 4");
+        }
+
+        else if (points < 79 && points > 70) {
+            System.out.println("Grade: 3");
+        }
+
+        else if (points < 69 && points > 60) {
+            System.out.println("Grade: 2");
+        }
+
+        else if (points < 59 && points > 50) {
+            System.out.println("Grade: 1");
+        }
+
+        else if (points < 49 && points > 0) {
+            System.out.println("Failed");
+        }
+
+        else if (points <= 0) {
+            System.out.println("Grade impossible!");
         }
     }
     
