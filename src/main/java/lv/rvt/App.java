d4403baa5ba1199307dc42c90bdd236ca0fb6c7b
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-10-10 11:35:05 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,32 +5,27 @@ import java.util.*;
 public class App 
 {
     public static void main( String[] args )
+
     {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Value of gift?");
-        int value = Integer.valueOf(scanner.nextLine());
-        double tax;
+    Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int amt = 0;
 
-        if (value >= 5000 && value < 25000) {
-            tax = (100 + (value - 5000) * 0.08);
-            System.out.println(tax);
-        } else if (value >= 2500 && value < 55000) {
-            tax = (1700 + (value - 25000) * 0.10);
-            System.out.println(tax);
-        } else if (value >= 55000 && value < 200000) {
-            tax = (4700 + (value - 55000) * 0.12);
-            System.out.println(tax);
-        } else if (value >= 200000 && value < 1000000) {
-            tax = (22100 + (value - 20000) * 0.15);
-            System.out.println(tax); 
-        } else if (value >= 1000000) {
-            tax = (142100 + (value - 1000000) * 0.17);
-            System.out.println(tax); 
-        }  else {
-            System.out.println("No tax!");
+        while (true) {
+            System.out.println("Give a number: ");
+
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                System.out.println("Number of numbers: " + amt );
+                    System.out.println("Sum of the numbers: " + sum);
+                        break;
+                }
+                else {
+                    sum = sum + number;
+                    amt = amt + 1;
+                }
         }
-
     }
-    
 }
