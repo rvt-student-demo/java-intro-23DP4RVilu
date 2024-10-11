d4c6d2ad7aec2389efd518fcb83a7ef31a4c0e06
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-10-11 10:10:24 +0000

    5 part excersise

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,19 +7,45 @@ public class App
     public static void main( String[] args )
 
     {
-
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int amt = 0;
+        double avg = 0.0;
+        int odd = 0;
+        int even = 0;
 
         System.out.println("Give a number: ");
 
-        Integer num = Integer.valueOf(scanner.nextLine());
-        
-        int fact = 1;
-    
-        for (int i = 1; i <= num; i++) {
-            fact = i * fact;
-            System.out.println(i);
+        while (true) {
+
+
+            Integer number = Integer.valueOf(scanner.nextLine());
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                System.out.println("Sum: " + sum);
+                System.out.println("Numbers: " + amt );
+                Double doublenumber = Double.valueOf(number);
+
+                if (amt > 0) {
+                    avg = (double) sum / amt;
+
+                System.out.println("Average: " + avg);
+                
+                }
+                if (number % 2 == 0) {
+                    even++;
+                } if (!(number % 2 == 0)) {
+                    odd++;
+                }
+                System.out.println("Even: " + even);
+                System.out.println("Odd: " + odd);
+
+                break;
+                }
+                else {
+                    sum = sum + number;
+                    amt = amt + 1;
+                }
         }
     }
 }
-
