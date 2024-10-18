5d07b7d341d692d335d56d141467fe9998648c92
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-10-18 10:53:35 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,21 +2,48 @@ package lv.rvt;
 
 import java.util.*;
 
-public class App 
-{
+public class App {
 
     public static void main(String[] args) {
-        divisibleByThreeInRange(2, 10);
-    
-        Scanner scanner = new Scanner(System.in);
+
+        printStars(3);
+        System.out.println(" ");
+        printSquare(4);
+        System.out.println(" ");
+        printRectangle(5, 6);
+        System.out.println(" ");
+        printTriangle(3);
+        System.out.println(" ");
     }
 
-    public static void divisibleByThreeInRange(int beginning, int end)
-    {
-    for (int iteration = beginning; iteration <= end; iteration++){
-        if(iteration % 3 == 0) {
-            System.out.println(iteration);
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
         }
-    }  
+        System.out.println("");
+    }
+
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            printStars(width);
+        } 
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
