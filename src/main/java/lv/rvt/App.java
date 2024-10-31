53566be1c02bd8b19b4c325501a0169287917f0b
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-10-31 13:15:59 +0000

    Chapter 60 Excercise 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,12 +6,19 @@ public class App {
 
     public static void main ( String[] args )
     {
-      int[] val = {0, 1, 2, 3}; 
+      int[] val = {13, -4, 82, 17}; 
+      int[] twice = new int[4];
+
+
+      System.out.println( "Original Array: " 
+          + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+   
+      for (int i = 0; i < val.length; i++) {
+        twice[i] = val[i] *= 2;
+      }
   
-      int sum = val[0] + val[1] + val[2] + val[3];
-   
-      System.out.println( "Sum of all numbers = " + sum );
-   
+      System.out.println( "New Array: " 
+          + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
      }
 }
 
