22f6926731f2e3a6248fca53d0136459ae611205
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-11-01 10:12:52 +0000

    Chapter 60 Excercise 3

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,21 +4,22 @@ import java.util.*;
 
 public class App {
 
-    public static void main ( String[] args )
-    {
-      int[] val = {13, -4, 82, 17}; 
-      int[] twice = new int[4];
-
-
-      System.out.println( "Original Array: " 
-          + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-   
-      for (int i = 0; i < val.length; i++) {
-        twice[i] = val[i] *= 2;
-      }
+public static void main ( String[] args )
+{
+  int[] valA   = { 13, -22,  82,  17}; 
+  int[] valB   = {-12,  24, -79, -13};
+  int[] sum    = {  0,   0,   0,   0};
   
-      System.out.println( "New Array: " 
-          + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
-     }
+  // Add values from corresponding cells of valA and valB
+  // and put the result in the corresponding cell of sum.
+ for (int i = 0; i < sum.length; i++) {
+      sum[i] = valA[i] + valB[i]; 
+ }
+
+
+
+  System.out.println( "sum: " 
+      + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+ }
 }
 
