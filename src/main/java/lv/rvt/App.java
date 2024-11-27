21d6b55e8d3e318b2c8ad8fecbd3ecb5cd68b4c8
23DP4RVilu <166484776+23DP4RVilu@users.noreply.github.com>
2024-11-27 16:49:28 +0000

    Payment card

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,14 +2,29 @@ package lv.rvt;
 
 public class App {
 
-  public static void main(String[] args) {
-    Statistics statistics = new Statistics();
-    statistics.addNumber(3);
-    statistics.addNumber(5);
-    statistics.addNumber(1);
-    statistics.addNumber(2);
-    System.out.println("Count: " + statistics.getCount());
-    System.out.println("Sum: " + statistics.sum());
-    System.out.println("Average: " + statistics.average());
-  }
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+
+        // Here you can write code to test that PaymentCard works as intended
+        // be sure to erase the extra code for the last part of the exercise!
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+        
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+        paulsCard.addMoney(20.00);
+        mattsCard.eatHeartily();
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+        
+        
+
+    }
 }
